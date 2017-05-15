9244c01e3b761a9f93f6c2434010753c790e155b
Hannes <koenigh@uchicago.edu>
2017-05-15 15:38:35 -0500

    updated visualization of decision tree function

--- exploration.py
+++ exploration.py
@@ -2,13 +2,35 @@
 Module for exploration functions.
 """
 
+import psycopg2
+import pydotplus
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
+
+from sklearn import tree
 from sklearn.model_selection import train_test_split
 
-
 plt.style.use('ggplot')
+
+def create_graph_for_decision_tree(X, y, max_depth):
+    """
+    Prints decision tree trained on X and y with max_depth max_depth.
+    In:
+        - X: training data
+        - y: targets
+        - max_depth: max_depth of tree
+    """
+
+    tree_clf = tree.DecisionTreeClassifier(max_depth=max_depth)
+    tree_clf = tree_clf.fit(X, y)
+
+    dot_data = tree.export_graphviz(tree_clf, out_file=None,
+                             feature_names=X.columns,
+                             filled=True, rounded=True,
+                             special_characters=True)
+    graph = pydotplus.graph_from_dot_data(dot_data)
+    return graph
 
 
 def plot_histograms(df, columns, min_max = False):
@@ -30,6 +52,30 @@ def plot_histograms(df, columns, min_max = False):
             df[col].plot.hist(alpha=0.5, title= col + ' histogram');
             plt.show();
 
+def read_data_from_db(query, conn_string, index_col=None, split=False, target=None, test_size=None):
+    """
+    Opens a connection with DB and runs query.
+    In:
+        - query: (str) SQL query
+        - conn_string: connection string to db; connection gets opened and closed by query
+        - index_col: index col for pd DF
+        - split: (bool) split data into train & test?
+        - target: column name of target
+        - test_size: % of test data
+    Out:
+        - data: pandas df
+    """
+    conn = psycopg2.connect(conn_string)
+
+    data = pd.read_sql_query(query, conn, index_col=index_col)
+    if split:
+        X_train, X_test, y_train, y_test = train_test_split(data.drop([target], axis=1),
+                                                            data[target], test_size=test_size,
+                                                            random_state=42)
+        return X_train, X_test, y_train, y_test
+
+    conn.close()
+    return data
 
 def read_data_from_csv(file_path, index_col=None, split=False, target=None, test_size=None):
     """
